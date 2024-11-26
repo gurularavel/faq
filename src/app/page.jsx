8bd77d48270044c0825f63299b3604ee216380b5
Nazrin Isgandarova <isgnezrin@gmail.com>
2024-11-26 13:22:14 +0400

    initial

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import LogoIcon from '@/assets/icons/logo.svg'
+
+export default function Home() {
+  return (
+    <>
+      <h2 className="text-6xl font-light">faq</h2>
+      {/* <LogoIcon/> */}
+    </>
+  );
+}
