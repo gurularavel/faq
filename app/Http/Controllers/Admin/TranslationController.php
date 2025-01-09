62bd734236e8e69cf655843b6e3e4ffa31260e0c
sahib fermanli <sahibfermanli230@gmail.com>
2025-01-09 18:40:36 +0400

    translation repository

--- app/Http/Controllers/Admin/TranslationController.php
+++ app/Http/Controllers/Admin/TranslationController.php
@@ -8,16 +8,21 @@ use App\Http\Requests\Admin\Translations\GetTranslationsRequest;
 use App\Http\Requests\Admin\Translations\TranslationStoreRequest;
 use App\Http\Requests\Admin\Translations\TranslationUpdateRequest;
 use App\Http\Resources\GeneralResource;
-use App\Models\Translation;
+use App\Repositories\TranslationRepository;
 use App\Services\LangService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use OpenApi\Annotations as OA;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class TranslationController extends Controller
 {
+    private TranslationRepository $repo;
+
+    public function __construct(TranslationRepository $repo)
+    {
+        $this->repo = $repo;
+    }
+
     /**
      * @OA\Get(
      *     path="/api/control/translations/load",
@@ -63,31 +68,7 @@ class TranslationController extends Controller
 
         $languages = LangService::instance()->getLanguages();
 
-        $sql_arr = [];
-        foreach ($languages as $language) {
-            $sql_arr[] = 'MAX(CASE WHEN language_id = ' . $language['id'] . ' THEN text END) AS lang_' . $language['key'];
-        }
-        $sql = implode(', ', $sql_arr);
-
-        $translations = Translation::query()
-            ->when($validated['group'] ?? null, function ($query) use ($validated) {
-                $query->where('group', $validated['group']);
-            })
-            ->when($validated['keyword'] ?? null, function ($query) use ($validated) {
-                $query->where('key', 'like', '%' . $validated['keyword'] . '%');
-            })
-            ->when($validated['text'] ?? null, function ($query) use ($validated) {
-                $query->where('text', 'like', '%' . $validated['text'] . '%');
-            })
-            ->groupBy(['group', 'key'])
-            ->orderBy('key')
-            ->orderBy('group')
-            ->select([
-                'group',
-                'key',
-                DB::raw($sql),
-            ])
-            ->get();
+        $translations = $this->repo->load($languages, $validated);
 
         return response()->json([
             'data' => [
@@ -167,24 +148,13 @@ class TranslationController extends Controller
      */
     public function show(string $group, string $key): JsonResponse
     {
-        $translations = Translation::query()
-            ->leftJoin('languages', 'translations.language_id', '=', 'languages.id')
-            ->where('translations.group', $group)
-            ->where('translations.key', $key)
-            ->select([
-                'translations.language_id',
-                'translations.group',
-                'translations.key',
-                'translations.text',
-                'languages.title as language',
-            ])
-            ->get();
+        $translations = $this->repo->findByKey($group, $key);
 
         if (count($translations) === 0) {
             return response()->json(GeneralResource::make([
                 'message' => 'Translations not found!',
             ]), 404);
-        }
+        };
 
         return response()->json([
             'data' => [
@@ -239,54 +209,13 @@ class TranslationController extends Controller
      */
     public function update(TranslationUpdateRequest $request, string $group, string $key): JsonResponse
     {
-        $translationsCount = Translation::query()
-            ->where('group', $group)
-            ->where('key', $key)
-            ->select('id')
-            ->count();
-
-        if ($translationsCount === 0) {
+        if (! $this->repo->checkByKey($group, $key)) {
             return response()->json(GeneralResource::make([
                 'message' => 'Translations not found!',
             ]), 404);
         }
 
-        $validated = $request->validated();
-        $translations_data = $validated['translations'];
-
-        DB::transaction(static function () use ($translations_data, $group, $key) {
-            foreach ($translations_data as $translation) {
-                if ($translation['group'] !== $group) {
-                    DB::rollBack();
-                    throw new BadRequestHttpException(
-                        LangService::instance()
-                            ->setDefault('Wrong translation group!')
-                            ->getLang('wrong_translation_group')
-                    );
-                }
-
-                if ($translation['key'] !== $key) {
-                    DB::rollBack();
-                    throw new BadRequestHttpException(
-                        LangService::instance()
-                            ->setDefault('Wrong key word!')
-                            ->getLang('wrong_translation_key_word')
-                    );
-                }
-
-                Translation::query()
-                    ->where('group', $group)
-                    ->where('key', $key)
-                    ->where('language_id', $translation['language_id'])
-                    ->update([
-                        'text' => $translation['text'],
-                    ]);
-
-                LangService::instance()->setTranslationsCache($group, $translation['language_id'], false);
-            }
-        });
-
-        LangService::instance()->changeTranslationVersion();
+        $this->repo->update($group, $key, $request->validated());
 
         return response()->json(GeneralResource::make([
             'message' => LangService::instance()
@@ -317,30 +246,12 @@ class TranslationController extends Controller
      */
     public function create(): JsonResponse
     {
-        $translations = [];
-
-        $languages = LangService::instance()->getLanguages();
-        $required = true;
-
-        foreach ($languages as $language) {
-            $translations[] = [
-                'language_id' => $language['id'],
-                'language' => $language['key'],
-                'text' => '',
-                'required' => $required
-            ];
-
-            if ($required) {
-                $required = false;
-            }
-        }
-
         return response()->json([
             'data' => [
                 'input' => [
                     'key' => '',
                     'group' => '',
-                    'translations' => $translations,
+                    'translations' => $this->repo->create(),
                 ],
                 'groups' => TranslationGroupEnum::getList(),
             ]
@@ -381,41 +292,18 @@ class TranslationController extends Controller
     {
         $validated = $request->validated();
 
-        $default_text = $validated['translations'][0]['text'];
-        $key = Str::slug($validated['key'], '_');
+        $validated['key'] = Str::snake($validated['key']);
 
-        $check_unique = Translation::query()
-            ->where([
-                'group' => $validated['group'],
-                'key' => $key,
-            ])
-            ->whereIn('language_id', data_get($validated['translations'], '*.language_id'))
-            ->exists();
-
-        if ($check_unique) {
+        if ($this->repo->isExists($validated['group'], $validated['key'], array_column($validated['translations'], 'language_id'))) {
             return response()->json(GeneralResource::make([
                 'message' => LangService::instance()
                     ->setDefault('This translation is exits!')
                     ->getLang('translation_is_exits'),
-                'key' => $key,
+                'key' => $validated['key'],
             ]), 400);
         }
 
-        DB::transaction(static function () use ($validated, $default_text, $key) {
-            foreach ($validated['translations'] as $translation) {
-                Translation::query()
-                    ->create([
-                        'group' => $validated['group'],
-                        'language_id' => $translation['language_id'],
-                        'key' => $key,
-                        'text' => $translation['text'] ?? $default_text,
-                    ]);
-
-                LangService::instance()->setTranslationsCache($validated['group'], $translation['language_id'], false);
-            }
-        });
-
-        LangService::instance()->changeTranslationVersion();
+        $this->repo->store($validated);
 
         return response()->json(GeneralResource::make([
             'message' => LangService::instance()
@@ -465,10 +353,7 @@ class TranslationController extends Controller
      */
     public function destroy(string $group, string $key): JsonResponse
     {
-        $translations = Translation::query()
-            ->where('group', $group)
-            ->where('key', $key)
-            ->get();
+        $translations = $this->repo->getByKey($group, $key);
 
         if (count($translations) === 0) {
             return response()->json(GeneralResource::make([
@@ -476,18 +361,7 @@ class TranslationController extends Controller
             ]), 404);
         }
 
-        DB::transaction(static function () use ($translations) {
-            foreach ($translations as $translation) {
-                $group = $translation->group;
-                $language_id = $translation->language_id;
-
-                $translation->delete();
-
-                LangService::instance()->setTranslationsCache($group, $language_id, false);
-            }
-        });
-
-        LangService::instance()->changeTranslationVersion();
+        $this->repo->destroy($translations);
 
         return response()->json(GeneralResource::make([
             'message' => LangService::instance()
---
+++ app/Repositories/TranslationRepository.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Translation;
+use App\Services\LangService;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class TranslationRepository
+{
+    public function load(array $languages, array $validated): Collection
+    {
+        $sql_arr = [];
+        foreach ($languages as $language) {
+            $sql_arr[] = 'MAX(CASE WHEN language_id = ' . $language['id'] . ' THEN text END) AS lang_' . $language['key'];
+        }
+        $sql = implode(', ', $sql_arr);
+
+        return Translation::query()
+            ->when($validated['group'] ?? null, function ($query) use ($validated) {
+                $query->where('group', $validated['group']);
+            })
+            ->when($validated['keyword'] ?? null, function ($query) use ($validated) {
+                $query->where('key', 'like', '%' . $validated['keyword'] . '%');
+            })
+            ->when($validated['text'] ?? null, function ($query) use ($validated) {
+                $query->where('text', 'like', '%' . $validated['text'] . '%');
+            })
+            ->groupBy(['group', 'key'])
+            ->orderBy('key')
+            ->orderBy('group')
+            ->select([
+                'group',
+                'key',
+                DB::raw($sql),
+            ])
+            ->get();
+    }
+
+    public function findByKey(string $group, string $key): Collection
+    {
+        return Translation::query()
+            ->leftJoin('languages', 'translations.language_id', '=', 'languages.id')
+            ->where('translations.group', $group)
+            ->where('translations.key', $key)
+            ->select([
+                'translations.language_id',
+                'translations.group',
+                'translations.key',
+                'translations.text',
+                'languages.title as language',
+            ])
+            ->get();
+    }
+
+    public function getByKey(string $group, string $key): Collection
+    {
+        return Translation::query()
+            ->where('group', $group)
+            ->where('key', $key)
+            ->get();
+    }
+
+    public function checkByKey(string $group, string $key): bool
+    {
+        return Translation::query()
+            ->where('group', $group)
+            ->where('key', $key)
+            ->select('id')
+            ->count();
+    }
+
+    public function isExists(string $group, string $key, array $languages): bool
+    {
+        return Translation::query()
+            ->where([
+                'group' => $group,
+                'key' => $key,
+            ])
+            ->whereIn('language_id', $languages)
+            ->exists();
+    }
+
+    public function create(): array
+    {
+        $translations = [];
+
+        $languages = LangService::instance()->getLanguages();
+        $required = true;
+
+        foreach ($languages as $language) {
+            $translations[] = [
+                'language_id' => $language['id'],
+                'language' => $language['key'],
+                'text' => '',
+                'required' => $required
+            ];
+
+            if ($required) {
+                $required = false;
+            }
+        }
+
+        return $translations;
+    }
+
+    public function store(array $validated): void
+    {
+        DB::transaction(static function () use ($validated) {
+            $defaultText = $validated['translations'][0]['text'];
+
+            foreach ($validated['translations'] as $translation) {
+                Translation::query()
+                    ->create([
+                        'group' => $validated['group'],
+                        'language_id' => $translation['language_id'],
+                        'key' => $validated['key'],
+                        'text' => $translation['text'] ?? $defaultText,
+                    ]);
+
+                LangService::instance()->setTranslationsCache($validated['group'], $translation['language_id'], false);
+            }
+        });
+
+        LangService::instance()->changeTranslationVersion();
+    }
+
+    public function update(string $group, string $key, array $validated): void
+    {
+        $translationsData = $validated['translations'];
+
+        DB::transaction(static function () use ($translationsData, $group, $key) {
+            foreach ($translationsData as $translation) {
+                if ($translation['group'] !== $group) {
+                    DB::rollBack();
+                    throw new BadRequestHttpException(
+                        LangService::instance()
+                            ->setDefault('Wrong translation group!')
+                            ->getLang('wrong_translation_group')
+                    );
+                }
+
+                if ($translation['key'] !== $key) {
+                    DB::rollBack();
+                    throw new BadRequestHttpException(
+                        LangService::instance()
+                            ->setDefault('Wrong key word!')
+                            ->getLang('wrong_translation_key_word')
+                    );
+                }
+
+                Translation::query()
+                    ->where('group', $group)
+                    ->where('key', $key)
+                    ->where('language_id', $translation['language_id'])
+                    ->update([
+                        'text' => $translation['text'],
+                    ]);
+
+                LangService::instance()->setTranslationsCache($group, $translation['language_id'], false);
+            }
+        });
+
+        LangService::instance()->changeTranslationVersion();
+    }
+
+    public function destroy(Collection $translations): void
+    {
+        DB::transaction(static function () use ($translations) {
+            foreach ($translations as $translation) {
+                $group = $translation->group;
+                $language_id = $translation->language_id;
+
+                $translation->delete();
+
+                LangService::instance()->setTranslationsCache($group, $language_id, false);
+            }
+        });
+
+        LangService::instance()->changeTranslationVersion();
+    }
+}
