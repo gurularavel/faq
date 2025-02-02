a5702a1d1276072dfccf121a494051c1034da696
Jamal Huseynov <chuseynov4@gmail.com>
2025-02-02 22:15:17 +0400

    destination

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, "src/"),
@@ -16,8 +15,5 @@ export default defineConfig({
       "@utils": path.resolve(__dirname, "src/utils/"),
       "@hooks": path.resolve(__dirname, "src/hooks/"),
     },
-  },
-  server: {
-    host: true,
   },
 });
