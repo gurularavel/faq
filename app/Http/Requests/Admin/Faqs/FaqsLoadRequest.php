dfab474c24b1a0c6498a7e5322dae05de2dd9be0
sahib fermanli <sahibfermanli230@gmail.com>
2025-02-06 15:37:20 +0400

    admin: faq filters - swagger generated

--- app/Http/Requests/Admin/Faqs/FaqsLoadRequest.php
+++ app/Http/Requests/Admin/Faqs/FaqsLoadRequest.php
@@ -5,6 +5,38 @@ namespace App\Http\Requests\Admin\Faqs;
 use App\Http\Requests\GeneralListRequest;
 use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Schema(
+ *     schema="FaqsLoadRequest",
+ *     type="object",
+ *     title="FAQs Load Request",
+ *     description="Request parameters for loading FAQs",
+ *     @OA\Property(
+ *         property="category",
+ *         type="integer",
+ *         description="ID of the category",
+ *         example=1
+ *     ),
+ *     @OA\Property(
+ *         property="status",
+ *         type="integer",
+ *         description="Status of the FAQ (1 - active, 2 - deactive)",
+ *         example=1
+ *     ),
+ *     @OA\Property(
+ *         property="search",
+ *         type="string",
+ *         description="Search term for FAQs",
+ *         example="example search term"
+ *     ),
+ *     @OA\Property(
+ *         property="limit",
+ *         type="integer",
+ *         description="Number of FAQs to load",
+ *         example=10
+ *     )
+ * )
+ */
 class FaqsLoadRequest extends GeneralListRequest
 {
     /**
