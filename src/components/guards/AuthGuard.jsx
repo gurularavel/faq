4bac9a1901883c04f5a12cea6eb3c64a5d70c464
Jamal Huseynov <chuseynov4@gmail.com>
2025-02-12 11:00:48 +0400

    auth guard

--- src/components/guards/AuthGuard.jsx
+++ src/components/guards/AuthGuard.jsx
@@ -2,11 +2,11 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function AuthGuard() {
-  // const { isLoggedIn } = useSelector((state) => state.auth);
+  const { isLoggedIn } = useSelector((state) => state.auth);
 
-  // if (!isLoggedIn) {
-  //   return <Navigate to="/auth/login" replace />;
-  // }
+  if (!isLoggedIn) {
+    return <Navigate to="/auth/login" replace />;
+  }
 
   return <Outlet />;
 }
