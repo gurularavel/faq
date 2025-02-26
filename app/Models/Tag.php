4c067c54f55d201297a135dbbc8f3452f858e7f9
sahib fermanli <sahibfermanli230@gmail.com>
2025-02-26 16:39:39 +0400

    tag CascadeSoftDeletes

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\ActionBy;
 use App\Traits\ActionUser;
 use App\Traits\SoftDeleteAcceptable;
+use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -15,13 +16,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Tag extends Model
 {
-    use SoftDeletes, ActionBy, ActionUser, SoftDeleteAcceptable;
+    use SoftDeletes, ActionBy, ActionUser, CascadeSoftDeletes;
 
     protected $fillable = [
         'title',
     ];
 
-    protected array $softDeleteAcceptableRelations = ['faqs'];
+    protected array $cascadeDeletes = ['faqs'];
 
     public function scopeActive(Builder $query): void
     {
