6c9a2e8e1a48ac81ce14933ea45e98a99587cd64
sahib fermanli <sahibfermanli230@gmail.com>
2025-10-31 23:10:08 +0400

    web.php fixed

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', static function () {
-    $settings = \App\Services\SettingService::instance()->get();
-    dd($settings);
     return 'Access denied!';
 });
